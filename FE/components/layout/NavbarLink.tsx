e1759b4aab072e3948472f120e97479559dcc643
JakeBarr20 <jakebiddibarr@hotmail.com>
2024-08-01 15:01:41 +0100

    adding in new navbar components

--- FE/components/layout/NavbarLink.tsx
+++ FE/components/layout/NavbarLink.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 interface NavbarLinkProps {
-  icon: string;
+  icon: any;
   text: string;
   isMobile: boolean;
   logout?: boolean;
